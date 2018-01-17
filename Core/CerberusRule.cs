ab2d1e33dfec8f0ea927dbe0118ffd54d91464a0
Benjamin Höglinger <nefarius@dhmx.at>
2018-01-17 02:30:40 +0100

    Logging fix Added Rule skeleton

---
+++ Core/CerberusRule.cs
@@ -0,0 +1,11 @@
+﻿namespace HidCerberus.Srv.Core
+{
+    public class CerberusRule
+    {
+        public string HardwareId { get; set; }
+
+        public bool IsAllowed { get; set; }
+
+        public bool IsPermanent { get; set; }
+    }
+}
