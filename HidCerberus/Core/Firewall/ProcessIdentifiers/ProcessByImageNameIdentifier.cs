1c0dbf96e3138a319be14f9eb82ad64459f6e120
Benjamin Höglinger <nefarius@dhmx.at>
2018-02-02 12:42:59 +0100

    Implemented basic process identifiers

---
+++ HidCerberus/Core/Firewall/ProcessIdentifiers/ProcessByImageNameIdentifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace HidCerberus.Core.Firewall.ProcessIdentifiers
+{
+    [UsedImplicitly]
+    public class ProcessByImageNameIdentifier : IProcessIdentifier
+    {
+        public string ImageName { get; set; }
+
+        public bool IdentifyByPid(int pid)
+        {
+            return ImageName.Equals(Process.GetProcessById(pid).ProcessName,
+                StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
---
+++ HidCerberus/Core/Firewall/ProcessIdentifiers/ProcessByImagePathIdentifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace HidCerberus.Core.Firewall.ProcessIdentifiers
+{
+    [UsedImplicitly]
+    public class ProcessByImagePathIdentifier : IProcessIdentifier
+    {
+        public string ImagePath { get; set; }
+
+        public bool IdentifyByPid(int pid)
+        {
+            return ImagePath.Equals(Process.GetProcessById(pid).MainModule.FileName,
+                StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
